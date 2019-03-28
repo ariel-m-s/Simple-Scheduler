6c13204c9134206ca3ec577eaef54bd03ad3da50
Ariel Martínez <amartinez5@uc.cl>
2019-03-28 18:53:03 -0300

    Implement stats init

--- src/stats/stats.c
+++ src/stats/stats.c
@@ -9,5 +9,10 @@ Stats *new_stats()
 {
   Stats *stats = calloc(1, sizeof(Stats));
 
+  stats->cpu_count = 0;
+  stats->interruption_count = 0;
+  stats->turnaround_time = 0;
+  stats->waiting_time = 0;
+
   return stats;
 }
